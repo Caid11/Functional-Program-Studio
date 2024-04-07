d4d6a3196744044974c38b6d7153ad110a6d6404
Kincaid Savoie <kincaidsavoie@gmail.com>
2024-04-06 18:12:05 -0600

    implemented select and deleted sleeps

--- blockbuilder/socket/socket-inline.c
+++ blockbuilder/socket/socket-inline.c
@@ -114,10 +114,8 @@ static uint8_t* kk_vector_to_bytes(kk_vector_t vec, int* length, kk_context_t* c
 
     uint8_t* bytes = (uint8_t*)malloc(kk_length);
 
-    for (kk_ssize_t i = 0; i < kk_length; i++) {
-        // TODO: KK_BORROWED or KK_OWNED?
+    for (kk_ssize_t i = 0; i < kk_length; i++)
         bytes[i] = kk_char_unbox(ptr[i], KK_BORROWED, ctx);
-    }
 
     kk_vector_drop(vec,ctx);
 
@@ -158,4 +156,90 @@ kk_std_core_types__tuple2 kk_socket_recv(int64_t socket, kk_integer_t kk_num_byt
 kk_integer_t kk_socket_shutdown(int64_t socket, kk_context_t* ctx) {
     int res = shutdown(socket, SD_SEND);
     return kk_integer_from_int32(res, ctx);
+}
+
+static fd_set kk_list_to_fd_set(kk_std_core_types__list kk_fds, kk_context_t* ctx) {
+    fd_set fds;
+    FD_ZERO(&fds);
+
+    kk_std_core_types__list fds_it = kk_fds;
+    while (kk_std_core_types__is_Cons(fds_it,ctx)) {
+        struct kk_std_core_types_Cons* cons = kk_std_core_types__as_Cons(fds_it,ctx);
+
+        SOCKET socket = kk_int64_unbox(cons->head, KK_BORROWED, ctx);
+        FD_SET(socket, &fds);
+
+        fds_it = cons->tail;
+    }
+
+    return fds;
+}
+
+static kk_std_core_types__list fd_set_to_kk_list(kk_std_core_types__list kk_fds, fd_set fds, kk_context_t* ctx) {
+    // Collect sockets to check
+
+    int num_sockets_to_check = 0;
+    SOCKET sockets_to_check[255];
+
+    kk_std_core_types__list fds_it = kk_fds;
+    while (kk_std_core_types__is_Cons(fds_it,ctx)) {
+        struct kk_std_core_types_Cons* cons = kk_std_core_types__as_Cons(fds_it,ctx);
+
+        SOCKET socket = kk_int64_unbox(cons->head, KK_BORROWED, ctx);
+        sockets_to_check[num_sockets_to_check++] = socket;
+
+        fds_it = cons->tail;
+    }
+    
+    // Construct list with all sockets from sockets_to_check that are in fds
+
+    kk_std_core_types__list nil  = kk_std_core_types__new_Nil(ctx);
+    struct kk_std_core_types_Cons* cons = NULL;
+    kk_std_core_types__list list = kk_std_core_types__new_Nil(ctx);
+
+    for (int i = 0; i < num_sockets_to_check; i++) {
+        if (!FD_ISSET(sockets_to_check[i], &fds))
+            continue;
+
+        kk_box_t boxed_socket = kk_int64_box(sockets_to_check[i], ctx);
+        kk_std_core_types__list head = kk_std_core_types__new_Cons(kk_reuse_null,0,boxed_socket, nil, ctx);
+        if (cons == NULL)
+            list = head;
+        else
+            cons->tail = head;
+        
+        cons = kk_std_core_types__as_Cons(head,ctx);
+    }
+
+    return list;
+}
+
+kk_std_core_types__tuple2 kk_socket_select(kk_std_core_types__list kk_readfds,
+                                           kk_std_core_types__list kk_writefds,
+                                           kk_std_core_types__list kk_exceptfds,
+                                           bool block,
+                                           kk_context_t* ctx) {
+    // Create read and write sets
+    fd_set readfds = kk_list_to_fd_set(kk_readfds, ctx);
+    fd_set writefds = kk_list_to_fd_set(kk_writefds, ctx);
+    fd_set exceptfds = kk_list_to_fd_set(kk_exceptfds, ctx);
+
+    // Do select call
+
+    int result;
+    if (block) {
+        result = select(0, &readfds, &writefds, &exceptfds, NULL);
+    } else {
+        TIMEVAL t = {0};
+        result = select(0, &readfds, &writefds, &exceptfds, &t);
+    }
+
+    // Put read and write sets in result struct
+    kk_std_core_types__list kk_resultreadfds = fd_set_to_kk_list(kk_readfds, readfds, ctx);
+    kk_std_core_types__list kk_resultwritefds = fd_set_to_kk_list(kk_writefds, writefds, ctx);
+    kk_std_core_types__list kk_resultexceptfds = fd_set_to_kk_list(kk_exceptfds, exceptfds, ctx);
+
+    kk_socket_socket__selectResult kk_result_fds = kk_socket_socket__new_SelectResult(kk_reuse_null, 0, kk_resultreadfds, kk_resultwritefds, kk_resultexceptfds, ctx);
+
+    return kk_std_core_types__new_Tuple2(kk_integer_box(kk_integer_from_int32(result, ctx), ctx), kk_socket_socket__selectResult_box(kk_result_fds, ctx), ctx);
 }
